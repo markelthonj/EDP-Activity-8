762bda6969f556fae819998737fb60d54f7e8cfe
markelthonj <168424837+markelthonj@users.noreply.github.com>
2024-05-30 22:08:58 +0800

    Add files via upload

---
+++ EDP - Activity 8/mprtCorp/employee.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace mprtCorp
+{
+    partial class employee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.datagrid_output = new System.Windows.Forms.DataGridView();
+            this.getbtn = new System.Windows.Forms.Button();
+            this.postbtn = new System.Windows.Forms.Button();
+            this.salarytxt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.usernametxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.emailtxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.passtxt = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagrid_output)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.postbtn);
+            this.panel1.Controls.Add(this.salarytxt);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.datagrid_output);
+            this.panel1.Controls.Add(this.getbtn);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.usernametxt);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.emailtxt);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.passtxt);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(7, 6);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.panel1.Size = new System.Drawing.Size(955, 430);
+            this.panel1.TabIndex = 0;
+            // 
+            // datagrid_output
+            // 
+            this.datagrid_output.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.datagrid_output.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.datagrid_output.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagrid_output.Location = new System.Drawing.Point(133, 22);
+            this.datagrid_output.Margin = new System.Windows.Forms.Padding(4);
+            this.datagrid_output.Name = "datagrid_output";
+            this.datagrid_output.RowHeadersVisible = false;
+            this.datagrid_output.RowHeadersWidth = 100;
+            this.datagrid_output.Size = new System.Drawing.Size(747, 165);
+            this.datagrid_output.TabIndex = 14;
+            this.datagrid_output.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datagrid_output_CellContentClick);
+            // 
+            // getbtn
+            // 
+            this.getbtn.BackColor = System.Drawing.SystemColors.Highlight;
+            this.getbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getbtn.Location = new System.Drawing.Point(398, 195);
+            this.getbtn.Margin = new System.Windows.Forms.Padding(4);
+            this.getbtn.Name = "getbtn";
+            this.getbtn.Size = new System.Drawing.Size(222, 52);
+            this.getbtn.TabIndex = 12;
+            this.getbtn.Text = "GET";
+            this.getbtn.UseVisualStyleBackColor = false;
+            this.getbtn.Click += new System.EventHandler(this.getbtn_Click);
+            // 
+            // postbtn
+            // 
+            this.postbtn.BackColor = System.Drawing.SystemColors.Highlight;
+            this.postbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postbtn.Location = new System.Drawing.Point(748, 296);
+            this.postbtn.Margin = new System.Windows.Forms.Padding(4);
+            this.postbtn.Name = "postbtn";
+            this.postbtn.Size = new System.Drawing.Size(158, 52);
+            this.postbtn.TabIndex = 11;
+            this.postbtn.Text = "POST";
+            this.postbtn.UseVisualStyleBackColor = false;
+            this.postbtn.Click += new System.EventHandler(this.postbtn_Click);
+            // 
+            // salarytxt
+            // 
+            this.salarytxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salarytxt.Location = new System.Drawing.Point(531, 354);
+            this.salarytxt.Margin = new System.Windows.Forms.Padding(4);
+            this.salarytxt.Name = "salarytxt";
+            this.salarytxt.Size = new System.Drawing.Size(178, 28);
+            this.salarytxt.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(409, 354);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 24);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Salary";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(531, 284);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(409, 284);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 24);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Department";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 259);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // usernametxt
+            // 
+            this.usernametxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernametxt.Location = new System.Drawing.Point(107, 255);
+            this.usernametxt.Margin = new System.Windows.Forms.Padding(4);
+            this.usernametxt.Name = "usernametxt";
+            this.usernametxt.Size = new System.Drawing.Size(213, 28);
+            this.usernametxt.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 312);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // emailtxt
+            // 
+            this.emailtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailtxt.Location = new System.Drawing.Point(103, 370);
+            this.emailtxt.Margin = new System.Windows.Forms.Padding(4);
+            this.emailtxt.Name = "emailtxt";
+            this.emailtxt.Size = new System.Drawing.Size(258, 28);
+            this.emailtxt.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 370);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email";
+            // 
+            // passtxt
+            // 
+            this.passtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passtxt.Location = new System.Drawing.Point(133, 308);
+            this.passtxt.Margin = new System.Windows.Forms.Padding(4);
+            this.passtxt.Name = "passtxt";
+            this.passtxt.Size = new System.Drawing.Size(229, 28);
+            this.passtxt.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Orange;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(7, 436);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.panel2.Size = new System.Drawing.Size(955, 0);
+            this.panel2.TabIndex = 1;
+            // 
+            // employee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(969, 442);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "employee";
+            this.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Text = "Activity 8 API";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagrid_output)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox emailtxt;
+        private System.Windows.Forms.TextBox passtxt;
+        private System.Windows.Forms.TextBox usernametxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button postbtn;
+        private System.Windows.Forms.Button getbtn;
+        private System.Windows.Forms.DataGridView datagrid_output;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox salarytxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
